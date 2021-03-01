90518f3fe54da2d3e91bc18f8ebbd98e6f32f404
Dima <dmitry.turov374@gmail.com>
2021-03-01 22:45:13 +0300

    add github fragment

--- app/src/main/java/com/example/githubusers/GitHubUsersActivity.kt
+++ app/src/main/java/com/example/githubusers/GitHubUsersActivity.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class GitHubUsersActivity : AppCompatActivity() {
+
+    private val fragmentManager = supportFragmentManager
+
+    private var githubUsersFragment = GithubUsersFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_git_hub_users)
+
+        fragmentManager.beginTransaction()
+            .add(R.id.container, githubUsersFragment)
+            .commit()
     }
 }
