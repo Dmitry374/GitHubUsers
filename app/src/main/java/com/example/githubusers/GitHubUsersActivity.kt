2e756521fa2eeeae07a550bb3730dc36b029ce9d
Dima <dmitry.turov374@gmail.com>
2021-03-01 21:40:16 +0300

    create splash screen

---
+++ app/src/main/java/com/example/githubusers/GitHubUsersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.githubusers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GitHubUsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_hub_users)
+    }
+}
